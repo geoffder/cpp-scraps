639f9bfddaf974bbecf15aed335ecce673208d23
Geoff deRosenroll <geoffderosenroll@gmail.com>
2023-07-13 12:30:14 -0700

    google clang format style

--- SlowHull/src/SlowHull.cpp
+++ SlowHull/src/SlowHull.cpp
@@ -1,4 +1,5 @@
 #include "SlowHull.h"
+
 #include <unordered_set>
 
 double DistanceToVector(const glm::vec3 &pt, const glm::vec3 &v) {
@@ -13,8 +14,7 @@ glm::vec4 PlaneOfPts(const glm::vec3 &a, const glm::vec3 &b,
                      const glm::vec3 &c) {
   auto crx = glm::cross(c - a, b - a);
   auto n = glm::length(crx);
-  if (n == 0.)
-    return glm::vec4();
+  if (n == 0.) return glm::vec4();
   return glm::vec4(crx, glm::dot(crx, a)) / n;
 }
 
@@ -32,8 +32,7 @@ glm::ivec3 NonCollinearTriple(const std::vector<glm::vec3> &pts,
       dist = d;
     }
   }
-  if (dist <= precision)
-    return glm::ivec3(-1);
+  if (dist <= precision) return glm::ivec3(-1);
   const auto n = (pts[0] - pts[furthest]) / dist;
   int third = -1;
   double offset = dist * precision;
@@ -44,8 +43,7 @@ glm::ivec3 NonCollinearTriple(const std::vector<glm::vec3> &pts,
       offset = off;
     }
   }
-  if (third < 0)
-    return glm::ivec3(-1);
+  if (third < 0) return glm::ivec3(-1);
   return glm::ivec3(0, furthest, third);
 }
 
@@ -54,10 +52,9 @@ std::pair<int, bool> NonCoplanar(const std::vector<glm::vec3> &pts,
                                  const double precision) {
   for (int i = 0; i < pts.size(); i++) {
     auto dist = DistanceToPlane(pts[i], plane);
-    if (std::abs(dist) > precision)
-      return {i, dist > precision};
+    if (std::abs(dist) > precision) return {i, dist > precision};
   }
-  return {-1, false}; // All points are coplanar, hull is not a 3d shape
+  return {-1, false};  // All points are coplanar, hull is not a 3d shape
 }
 
 struct IVec2Hash {
@@ -70,13 +67,11 @@ std::vector<glm::ivec3> Hull(std::vector<glm::vec3> pts,
                              const double precision) {
   const int len = pts.size();
   // FIXME: Should there be a hull specific error?
-  if (len < 4)
-    return std::vector<glm::ivec3>{};
+  if (len < 4) return std::vector<glm::ivec3>{};
   const auto trip = NonCollinearTriple(pts, precision);
   const auto plane = PlaneOfPts(pts[trip.x], pts[trip.y], pts[trip.z]);
   const auto [d, dIsAboveTrip] = NonCoplanar(pts, plane, precision);
-  if (d < 0)
-    return std::vector<glm::ivec3>{}; // All points coplanar.
+  if (d < 0) return std::vector<glm::ivec3>{};  // All points coplanar.
   const int a = trip.x;
   const int b = dIsAboveTrip ? trip.z : trip.y;
   const int c = dIsAboveTrip ? trip.y : trip.z;
@@ -106,8 +101,7 @@ std::vector<glm::ivec3> Hull(std::vector<glm::vec3> pts,
   AddTri(c, d, a);
   AddTri(b, d, c);
   for (int i = 0; i < len; i++) {
-    if (i == a || i == b || i == c || i == d)
-      continue; // skip starting points
+    if (i == a || i == b || i == c || i == d) continue;  // skip starting points
     // collect half edges of triangles that are in conflict with the points at
     // idx, pruning the conflicting triangles and their planes in the process
     for (int j = 0; j < triangles.size(); j++) {
@@ -123,16 +117,14 @@ std::vector<glm::ivec3> Hull(std::vector<glm::vec3> pts,
     // form new triangles with the outer perimeter (horizon) of the set of
     // conflicting triangles and the point at idx
     for (auto e : halfEdges) {
-      if (halfEdges.erase({e.y, e.x}) == 0)
-        AddTri(e.x, e.y, i);
+      if (halfEdges.erase({e.y, e.x}) == 0) AddTri(e.x, e.y, i);
     }
     halfEdges.clear();
   }
   std::vector<glm::ivec3> out;
   out.reserve((triangles.size() - dropped.size()) * 3);
   for (int i = 0; i < triangles.size(); i++) {
-    if (!kept[i])
-      continue;
+    if (!kept[i]) continue;
     out.push_back(triangles[i]);
   }
   return out;
@@ -142,12 +134,9 @@ std::vector<glm::ivec3> Hull(const std::vector<glm::vec3> &pts) {
   float scale = 0;
   for (int i = 0; i < pts.size(); i++) {
     auto abs = glm::abs(pts[i]);
-    if (abs.x > scale)
-      scale = abs.x;
-    if (abs.y > scale)
-      scale = abs.y;
-    if (abs.z > scale)
-      scale = abs.z;
+    if (abs.x > scale) scale = abs.x;
+    if (abs.y > scale) scale = abs.y;
+    if (abs.z > scale) scale = abs.z;
   }
   return Hull(pts, 1e-9 * scale);
 }
